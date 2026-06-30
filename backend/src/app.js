5195b447d28ba38068d28dcd5342d9a7f6ebb41f
kundan3238-exe <kundan.atel8899@gmail.com>
2026-06-30 21:53:55 +0530

    Setup Express server and MongoDB connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,23 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import Routes from "./routes/Routes.js";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use("/api", Routes);
 
 export default app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import dotenv from "dotenv";
-import app from "./app.js";
-
 dotenv.config();
+import http from "http";
+import app from "./app.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
+connectDB();
+const server = http.createServer(app);
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
