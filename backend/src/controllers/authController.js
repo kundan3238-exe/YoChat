bade5559b60b29c446547784cb82ca0ee96936df
kundan3238-exe <kundan.atel8899@gmail.com>
2026-07-02 00:53:42 +0530

    Bcrypt Used Successfully

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,11 +1,53 @@
-export const register = (req, res) => {
-  res.json({
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const register = async (req, res) => {
+  const { name, username, email, password } = req.body;
+
+  if (!name || !username || !email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
+
+  const existingUser = await User.findOne({
+    $or: [{ email }, { username }],
+  });
+
+  if (existingUser) {
+    return res.status(409).json({
+      success: false,
+      message: "Email or username already exists",
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("Original Password:", password);
+  console.log("Hashed Password:", hashedPassword);
+
+  const user = await User.create({
+    name,
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  res.status(200).json({
     success: true,
-    message: "Register controller working",
+    message: "Registered Successfully",
   });
 };
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields required",
+    });
+  }
   res.json({
     success: true,
     message: "Login controller working",
