4121721ae84df695dbc58e9ce055723f0275efb1
kundan3238-exe <kundan.atel8899@gmail.com>
2026-07-04 00:46:45 +0530

    feat(auth): implement AuthContext authentication logic

--- frontend/frontend/src/context/AuthContext.jsx
+++ frontend/frontend/src/context/AuthContext.jsx
@@ -5,13 +5,45 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
+
+  const checkAuth = async () => {
+    try {
+        const data = await authService.getCurrentUser()
+        setUser(data.user)
+    } catch (error) {
+        setUser(null)
+    }finally{
+        setLoading(false)
+    }
+  }
+
+const login = async (credentials) => {
+    const data = await authService.login(credentials)
+    setUser(data.user)
+    return data
+}
+
+const register = async (userData) => {
+    const data = await authService.register(userData)
+    setUser(data.user)
+    return data
+}
+
+const logout = async () => {
+    await authService.logout()
+    setUser(null)
+}
 
   return (
     <AuthContext.Provider
       value={{
         user,
-        setUser,
+        loading,
+        login,
+        register,
+        logout,
+        checkAuth
       }}
     >
       {children}
