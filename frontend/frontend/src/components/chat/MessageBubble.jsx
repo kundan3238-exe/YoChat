0c598ffb13151c027f80a0526109eaa52327704d
kundan3238-exe <kundan.atel8899@gmail.com>
2026-07-09 00:10:46 +0530

    feat: implement dynamic message rendering

--- frontend/frontend/src/components/chat/MessageBubble.jsx
+++ frontend/frontend/src/components/chat/MessageBubble.jsx
@@ -1,15 +1,22 @@
 import useAuth from "../../hooks/useAuth";
 
 const MessageBubble = ({ message }) => {
-  const { authUser } = useAuth();
+  const { user } = useAuth();
 
-  const isMyMessage = message.senderId === authUser._id;
-
+  const isMyMessage = message.senderId === user?._id;
+// console.log(message.senderId);
+// console.log(user?._id);
   return (
-    <div className={`flex ${isMyMessage ? "justify-end" : "justify-start"}`}>
+    <div
+      className={`flex ${
+        isMyMessage ? "justify-end" : "justify-start"
+      }`}
+    >
       <div
         className={`max-w-xs px-4 py-2 rounded-2xl ${
-          isMyMessage ? "bg-violet-600 text-white" : "bg-[#211D2C] text-white"
+          isMyMessage
+            ? "bg-violet-600 text-white"
+            : "bg-[#211D2C] text-white"
         }`}
       >
         {message.message}
