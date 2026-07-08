0a9a8d8501ba252fdf784c8237ec628e12c3bb7f
kundan3238-exe <kundan.atel8899@gmail.com>
2026-07-09 00:04:38 +0530

    fix: resolve auth hook import and continue chat rendering

--- frontend/frontend/src/components/chat/MessageBubble.jsx
+++ frontend/frontend/src/components/chat/MessageBubble.jsx
@@ -1,7 +1,19 @@
-const MessageBubble = () => {
+import useAuth from "../../hooks/useAuth";
+
+const MessageBubble = ({ message }) => {
+  const { authUser } = useAuth();
+
+  const isMyMessage = message.senderId === authUser._id;
+
   return (
-    <div className="max-w-xs bg-violet-600 text-white rounded-2xl px-4 py-2">
-      Hello 👋
+    <div className={`flex ${isMyMessage ? "justify-end" : "justify-start"}`}>
+      <div
+        className={`max-w-xs px-4 py-2 rounded-2xl ${
+          isMyMessage ? "bg-violet-600 text-white" : "bg-[#211D2C] text-white"
+        }`}
+      >
+        {message.message}
+      </div>
     </div>
   );
 };
--- frontend/frontend/src/hooks/useAuth.js
+++ frontend/frontend/src/hooks/useAuth.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 
-const useAuth = () =>{
-    return useContext(AuthContext)
-}
-export default useAuth
+const useAuth = () => {
+  return useContext(AuthContext);
+};
+export default useAuth;
